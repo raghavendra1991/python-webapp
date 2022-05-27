0307bcdd60f07c125d6d1fd15a8b0f9d31b5b52e
root <root@docker.yns1e5uogp1e1deaxwjdxf2boc.tx.internal.cloudapp.net>
2022-05-27 02:22:53 +0000

    second commit

--- src_data/app.py
+++ src_data/app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from markupsafe import escape
+from flask import Flask, abort
 
 PORT = 8000
-MESSAGE = "Hello, world duvva raghavendra!\n"
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def root():
-    result = MESSAGE.encode("utf-8")
-    return result
+@app.route('/')
+@app.route('/index/')
+def hello():
+    return '<h1>Hello, World!</h1>'
 
 
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return '<h1>{}</h1>'.format(n1 + n2)
+
+@app.route('/users/<int:user_id>/')
+def greet_user(user_id):
+    users = ['Bob', 'Jane', 'Adam']
+    try:
+        return '<h2>Hi {}</h2>'.format(users[user_id])
+    except IndexError:
+        abort(404)
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
